3759d610790a2899f707686663b98273f9a115ce
Vishnuvelragavan <11229a051@kanchiuniv.ac.in>
2025-04-24 15:41:29 +0530

    subnet.py

---
+++ subnet.py
@@ -0,0 +1,19 @@
+import ipaddress
+
+def subnet_info(ip, subnet):
+    network = ipaddress.IPv4Network(f"{ip}/{subnet}", strict=False)
+    print("Network Address:", network.network_address)
+    print("Broadcast Address:", network.broadcast_address)
+    print("Subnet Mask:", network.netmask)
+    print("No. of Hosts:", network.num_addresses - 2)
+
+subnet_info("192.168.1.10", 24)
+
+
+OUTPUT:
+
+
+Network Address: 192.168.1.0
+Broadcast Address: 192.168.1.255
+Subnet Mask: 255.255.255.0
+No. of Hosts: 254
